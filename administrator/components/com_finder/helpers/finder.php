74ebbd40322d7688cccf179b602c7e3f870f0ac5
leo <leo@bm-sms.com.tw>
2014-02-19 15:36:59 +0800

    added Joomla! files

---
+++ administrator/components/com_finder/helpers/finder.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_finder
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Helper class for Finder.
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_finder
+ * @since       2.5
+ */
+class FinderHelper
+{
+	/**
+	 * @var		string	The extension name.
+	 * @since	2.5
+	 */
+	public static $extension = 'com_finder';
+
+	/**
+	 * Configure the Linkbar.
+	 *
+	 * @param   string  $vName  The name of the active view.
+	 *
+	 * @return  void
+	 *
+	 * @since   2.5
+	 */
+	public static function addSubmenu($vName)
+	{
+		JHtmlSidebar::addEntry(
+			JText::_('COM_FINDER_SUBMENU_INDEX'),
+			'index.php?option=com_finder&view=index',
+			$vName == 'index'
+		);
+		JHtmlSidebar::addEntry(
+			JText::_('COM_FINDER_SUBMENU_MAPS'),
+			'index.php?option=com_finder&view=maps',
+			$vName == 'maps'
+		);
+		JHtmlSidebar::addEntry(
+			JText::_('COM_FINDER_SUBMENU_FILTERS'),
+			'index.php?option=com_finder&view=filters',
+			$vName == 'filters'
+		);
+	}
+}
