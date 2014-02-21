28215313af11761ffc8582150736a139936d1815
leo <leo@bm-sms.com.tw>
2014-02-21 15:45:23 +0800

    show item list in a table

--- administrator/components/com_blog/views/items/tmpl/default.php
+++ administrator/components/com_blog/views/items/tmpl/default.php
@@ -6,9 +6,33 @@
       method="post"
       name="adminForm"
       id="adminForm">
-	<?php
-	var_dump($this->items);
-	?>
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th>
+					Title
+				</th>
+				<th>
+					Id
+				</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php foreach ($this->items as $i => $item): ?>
+			<tr>
+				<td>
+					<a href="<?php echo JRoute::_('index.php?option=com_blog&task=item.edit&id=' . $item->id); ?>">
+						<?php echo $this->escape($item->title); ?>
+					</a>
+				</td>
+				<td>
+					<?php echo $item->id; ?>
+				</td>
+			</tr>
+		<?php endforeach; ?>
+		</tbody>
+	</table>
+
 	<input type="hidden" name="option" value="com_blog"/>
 	<input type="hidden" name="task" value=""/>
 	<?php echo JHtml::_('form.token'); ?>
