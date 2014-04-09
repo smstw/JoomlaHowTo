71b336b6feeec3b67b00af2c5b1a9bf64354afa7
leo <leo@bm-sms.com.tw>
2014-04-09 14:49:48 +0800

    updated sakura edit page

--- administrator/components/com_flower/views/sakura/tmpl/edit.php
+++ administrator/components/com_flower/views/sakura/tmpl/edit.php
@@ -7,9 +7,28 @@
 	name="adminForm"
 	id="adminForm">
 
-	<?php var_dump($this->item); ?>
-	<?php var_dump($this->form); ?>
+	<div class="form-horizontal">
+		<div class="row-fluid">
+			<div class="span8">
+				<?php
+				foreach ($this->form->getFieldSet('basic') as $field)
+				{
+					echo $field->getControlGroup();
+				}
+				?>
+			</div>
+			<div class="span4">
+				<?php
+				foreach ($this->form->getFieldSet('datetime') as $field)
+				{
+					echo $field->getControlGroup();
+				}
+				?>
+			</div>
+		</div>
+	</div>
 
 	<input type="hidden" name="task" value=""/>
+	<input type="hidden" name="id" value="<?php echo $this->item->id; ?>"/>
 	<?php echo JHtml::_('form.token'); ?>
 </form>
